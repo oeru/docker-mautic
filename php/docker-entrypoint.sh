36d47cf83fb1c4d8a7f7cf4fd940c0639f302b8b
Dave Lane <dave@davelane.nz>
2018-02-15 10:03:04 +1300

    added nginx container, and nginx config to make forms generate.js and, I hope, the tracking pixel, etc.

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -48,23 +48,24 @@ if ! [ -e index.php -a -e app/AppKernel.php ]; then
 fi
 
 # run composer to set up dependencies if not already there...
+if ! [ -e /usr/local/bin/composer ]; then
+    echo >&2 "first getting Composer"
+    # Get Composer
+    curl -S https://getcomposer.org/installer | php
+    chmod a+x composer.phar
+    mv composer.phar /usr/local/bin/composer
+fi
 if ! [ -e vendor/autoload.php ]; then
     echo >&2 "installing dependencies with Composer"
-    if ! [ -e /usr/local/bin/composer ]; then
-        echo >&2 "first getting Composer"
-        # Get Composer
-        curl -S https://getcomposer.org/installer | php
-        chmod a+x composer.phar
-        mv composer.phar /usr/local/bin/composer
-    fi
     if ! [ -e .git/hooks ]; then
         echo >&2 "creating a .git/hooks dir to avoid errors"
         mkdir -p .git/hooks
     fi
-    composer install
 else
     echo >&2 "vendor dependencies already in place."
 fi
+echo >&2 "installing/updating vendor dependencies... running as user www-data"
+sudo -u www-data composer install
 
 # Ensure the MySQL Database is created
 php /makedb.php "$MAUTIC_DB_HOST" "$MAUTIC_DB_USER" "$MAUTIC_DB_PASSWORD" "$MAUTIC_DB_NAME"
